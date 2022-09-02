fb127c6b1352c4f73be8ac644ab05819e6d26e9f
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-09-02 15:40:27 +1200

    added gutter utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./app/**/*.{ts,tsx,jsx,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				gutter: "var(--gutter)",
+			},
+		},
 	},
 	plugins: [],
 };
